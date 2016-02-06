7d00e2ef4a9a848cabee36798891fbc0125c60a1
polarsbear <lveelaert@gmail.com>
2016-02-06 16:18:35 +0100

    initial

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+home=os.getenv("HOME")
+#calendarfile='/data/data/com.termux/files/home/DATA/PYCAL/calendar.txt'
+#2 calendarfile='/storage/sdcard1/Android/data/com.termux/files/DATA/PYCAL/calendar.txt'
+calendarfile=home+'/DATA/Calendar/calendar.txt'
+
